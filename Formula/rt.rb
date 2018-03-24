79200443016922d369d4e10ef8352479098d7972
Chip Wasson <chip@wasson.io>
2018-03-24 13:56:20 -0600

    Bump RT Version

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -1,7 +1,7 @@
 class Rt < Formula
   desc "A tool to install the rt tool from it's private repository."
   homepage ""
-  url "https://github.com/RadarRelay/rt-installer/archive/v0.4.tar.gz"
+  url "https://github.com/RadarRelay/rt-installer/archive/v0.5.tar.gz"
   sha256 "73ba742b0fbea0bc07d0c1e6837cb80ec05897ac7c5d24e8606cc1d72aa08f81"
 
   def install
