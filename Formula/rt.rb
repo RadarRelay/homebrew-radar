677112f45ddb16f8ae05b82a8ac06d2e9f86b972
Chip Wasson <chip@wasson.io>
2018-04-03 12:02:19 -0600

    Bump with fake installer version

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -1,8 +1,8 @@
 class Rt < Formula
   desc "A tool to install the rt tool from it's private repository."
   homepage ""
-  url "https://github.com/RadarRelay/rt-installer/archive/v0.7.tar.gz"
-  sha256 "7edf3224cf4d967a7bd380d2ee864721589e077537302d03b1f42c3d4c11a9b0"
+  url "https://github.com/RadarRelay/rt-installer/archive/v0.7.1.tar.gz"
+  sha256 "be44a38b943cfbe2ca0fc0a672ce4eafb56eee6b597f9c3dc0b06b3df54adec9"
 
   def install
     ENV.deparallelize  
