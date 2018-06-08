45774a21bf693e8deecfaee8d41d5933344271e4
Chip Wasson <chip@wasson.io>
2018-06-07 18:27:18 -0600

    Bump radar-star

--- Formula/radar-star.rb
+++ Formula/radar-star.rb
@@ -1,8 +1,8 @@
 class RadarStar < Formula
   desc "A command line tool similar to rt"
   homepage "https://github.com/RadarRelay/radar-star"
-  url "https://github.com/RadarRelay/radar-star/archive/v1.2.tar.gz"
-  sha256 "857843f6aa33b6290bb87f6d9f533b26a896200c4aa843dcd1d8c5f21af74407"
+  url "https://github.com/RadarRelay/radar-star/archive/v2.0.tar.gz"
+  sha256 "b3db88377f4f915b768bd546e9f07bee7e8b91776ad621f782902534714c3387"
 
   def install
     bin.install "star"
