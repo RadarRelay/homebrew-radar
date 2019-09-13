7d2b3ebdd9c7f61f54e5e9315c7f545d274dbb46
Chip Wasson <chip@wasson.io>
2019-09-13 17:31:49 -0600

    Explicit version

--- Formula/deploy.rb
+++ Formula/deploy.rb
@@ -3,6 +3,7 @@ class Deploy < Formula
     homepage "https://deploy.radar.tech"
     url "https://cli-prod-deploy.s3.us-east-2.amazonaws.com/deploy-v0.1.9/deploy-v0.1.9-darwin-x64.tar.gz"
     sha256 "c0a57572e5b06eda11c16270381f7b8420a745c56cf4648792c9c0b8aac47523"
+    version "v0.1.9"
   
     def install
       inreplace "bin/deploy", /^CLIENT_HOME=/, "export DEPLOY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
