b6902395286c802edec38d7fe10fc75887ae015e
Chip Wasson <chip@wasson.io>
2020-04-29 21:39:48 -0600

    Update from pre-release

--- Formula/rv.rb
+++ Formula/rv.rb
@@ -2,7 +2,7 @@ class Rv < Formula
   desc ""
   homepage "https://github.com/radartech/rv"
   url "https://rv.vault.rdr.sh/rv-v0.6.3/rv-v0.6.3-darwin-x64.tar.gz"
-  sha256 "8783795cd5c42c4285076c506d63c314efe6737db70dc9b0461e908be809c018"
+  sha256 "58554628b1835ff15d9a95a6ba052315a23dab2ce01b8afb6da2f6bb64cda4e5"
   version "v0.6.3"
 
   def install
